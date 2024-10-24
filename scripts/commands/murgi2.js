ce50bd9be52318ca8c89bfd2f2f32cb74aaaf579
MorshEd <mdmorshed4718@gmail.com>
2024-10-25 00:41:28 +0600

    Update murgi2.js

--- scripts/commands/murgi2.js
+++ scripts/commands/murgi2.js
@@ -17,7 +17,7 @@ module.exports.run = async function({ api, args, Users, event}) {
     var arraytag = [];
         arraytag.push({id: mention, tag: name});
     var a = function (a) { api.sendMessage(a, event.threadID); }
-a("আবাল লো আরিয়ান এর চুদা");
+a("আবাল লো MorshEd এর চুদা");
 setTimeout(() => {a({body: "খাংকির পোলা তর মারে চুদি 🥰।" + " " + name, mentions: arraytag})}, 3000);
 setTimeout(() => {a({body: "খাংকির পোলা তর কচি বোন রে চুদি 😍.." + " " + name, mentions: arraytag})}, 5000);
 setTimeout(() => {a({body: "মাদারচোদ তর আম্মু পম পম খাংকির পো 🐰" + " " + name, mentions: arraytag})}, 7000);
